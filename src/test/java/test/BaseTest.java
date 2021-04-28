80328e34deaf0906a1330b152235332e5a48991e
Biru Shrestha <biru.maka@gmail.com>
2021-04-28 20:11:14 +0545

    Remove test annotations

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 import page.LoginPage;
 
 import java.util.concurrent.TimeUnit;
